a83595d73f36e004bda2ade4970807a1f97d5e78
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-06-24 09:44:22 +0100

    Remove old yaml from SrGWAS

--- SrGwas/index_snps.py
+++ SrGwas/index_snps.py
@@ -1,18 +1,20 @@
-from miscSupports import validate_path, terminal_time, load_yaml
+from miscSupports import validate_path, terminal_time
 from csvObject import CsvObject, write_csv
 from bgen_reader import custom_meta_path
 from pysnptools.distreader import Bgen
-from random import sample
 from pathlib import Path
 
 
-def set_snp_ids(yaml_path, gen_path, write_dir, file_name):
+def set_snp_ids(memory_location, snps_to_id, gen_path, write_dir, file_name):
     """
     Isolate a subset of snps based on pre-defined named snps in a csv, passed as a str to snps_to_id, or a random
     set of snps of total == pre-defined int, where the int is set to snps_to_id.
 
-    :param yaml_path: The path to the yaml file
-    :type yaml_path: Path | str
+    :param memory_location: Location of bgen memory file
+    :type memory_location: Path | str
+
+    :param snps_to_id: Location of snps csv to id
+    :type snps_to_id: Path | str
 
     :param gen_path: The path to the genetic file
     :type gen_path: Path | str
@@ -30,24 +32,14 @@ def set_snp_ids(yaml_path, gen_path, write_dir, file_name):
     """
 
     # Load the args dict, then set the custom write location for the bgen file memory files and load the genetic ref
-    args = load_yaml(validate_path(yaml_path))
-    custom_meta_path(validate_path(args["memory_file_location"]))
+    custom_meta_path(validate_path(memory_location))
     gen = Bgen(str(validate_path(gen_path).absolute()))
 
     # If the snps_to_id is a str, assume a path and index the snps in the csv
-    if isinstance(args["snps_to_id"], str):
-        snps = CsvObject(validate_path(args["snps_to_id"]), set_columns=True)[0]
-        snp_list = [f"{snp},{snp}" for snp in snps]
-        snp_list = gen.sid_to_index(snp_list).tolist()
+    snps = CsvObject(validate_path(snps_to_id), set_columns=True)[0]
+    snp_list = [f"{snp},{snp}" for snp in snps]
+    snp_list = gen.sid_to_index(snp_list).tolist()
 
-    # If an int, get a random sample of snps of population sid_count snp_to_id times.
-    elif isinstance(args["snps_to_id"], int):
-        snp_list = sample(range(gen.sid_count), args["snps_to_id"])
-
-    # Else snp_to_id is invalid
-    else:
-        raise TypeError(f"set_snp_ids expects an int or a str yet was passed {type(args['snps_to_id'])}\n"
-                        f"If you want to run a GWAS with all snps, simply set snps: null and ignore this method")
-
+    # Write the snps out
     write_csv(write_dir, f"{file_name}_Snps", ["Snp"], snp_list)
     print(f"Constructed snp id list {terminal_time()}")
